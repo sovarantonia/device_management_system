45c402bc7f3e4e578481a92b9696a95eff000a95
sovarantonia <sovarantonia@yahoo.com>
2026-04-09 17:44:50 +0300

    fixed login redirection when refreshing the page

--- frontend/src/app/service/auth/auth-interceptor.ts
+++ frontend/src/app/service/auth/auth-interceptor.ts
@@ -1,13 +1,19 @@
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
-import { inject, Injectable } from "@angular/core";
+import { inject, Injectable, PLATFORM_ID } from "@angular/core";
 import { Observable } from "rxjs";
 import { AuthService } from "./auth-service";
+import { isPlatformBrowser } from "@angular/common";
 
 @Injectable({ providedIn: 'root' })
 export class AuthInterceptor implements HttpInterceptor {
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         const authService = inject(AuthService);
         const token = authService.getToken();
+        const platformId = inject(PLATFORM_ID);
+
+         if (!isPlatformBrowser(platformId)) {
+            return next.handle(req);
+        }
 
         const isPublicEndpoint =
             req.url.includes('/login') ||
